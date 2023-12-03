23b900121276017079e849c6b54e584ad85a2f50
reyhan muhammad <reyhanmuhammad@reyhans-MacBook-Air.local>
2023-12-03 07:52:16 +0700

    first commit

---
+++ Tap/GameScene.swift
@@ -0,0 +1,60 @@
+//
+//  GameScene.swift
+//  Tap
+//
+//  Created by reyhan muhammad on 03/12/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+        
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+    
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
